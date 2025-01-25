242f760fd9937a14ea41019f7dfe77f083699a8c
issa98 <isasarhan.lz@gmail.com>
2025-01-25 13:03:02 +0200

    setup

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { AssociationsModule } from './modules/associations/associations.module';
+import { DepartmentsModule } from './modules/departments/departments.module';
+import { AchievementsModule } from './modules/achievements/achievements.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [ConfigModule.forRoot(
+    {
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}.local`,
+    }
+  ), UsersModule, AssociationsModule, DepartmentsModule, AchievementsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
